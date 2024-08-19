290f7ff1d444749f0e00b417ee8e998af3606a17
DriverLin <lty6531600@gmail.com>
2024-08-19 13:19:13 +0800

    test all in one

--- nginx/conf.d/emby.js
+++ nginx/conf.d/emby.js
@@ -6,8 +6,8 @@ const debug = (msg) => {
 
 //修改FONT_IN_ASS_SERVER为你的Emby服务器地址
 //修改EMBY_SERVER_URL为你的字体处理服务器地址
-const FONT_IN_ASS_SERVER = "http://192.168.3.128:8011"
-const EMBY_SERVER_URL = "http://192.168.3.1:7096"
+const FONT_IN_ASS_SERVER = "http://192.168.3.3:8011"
+const EMBY_SERVER_URL = "http://127.0.0.1:7096"
 
 const subtitlesHandler = async (r) => {
     const rawResponse = await ngx.fetch(`${EMBY_SERVER_URL}${r.uri}${qs.stringify(r.args) === "" ? "" : "?" + qs.stringify(r.args)}`)
