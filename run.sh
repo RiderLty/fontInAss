62ce2b88c473f05973696f7940dd5f266fe3ea5e
RiderLty <135112135+RiderLty@users.noreply.github.com>
2024-08-19 09:01:51 +0000

    bug fix

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 if [ ! -f ./configure_nginx ]; then \
-    cat /etc/nginx/conf.d/emby.conf.template | envsubst > /etc/nginx/conf.d/emby.conf 
-    cat /etc/nginx/conf.d/emby.js.template | envsubst > /etc/nginx/conf.d/emby.js 
+    cat /etc/nginx/conf.d/emby.conf.template | envsubst '${EMBY_SERVER_URL}' > /etc/nginx/conf.d/emby.conf 
+    cat /etc/nginx/conf.d/emby.js.template | envsubst '${EMBY_SERVER_URL}' > /etc/nginx/conf.d/emby.js 
     touch ./configure_nginx
 fi
 nginx && python3 main.py
