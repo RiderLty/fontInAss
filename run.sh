5518bf69e7e794dcb87ced7a198353baf640200d
RiderLty <135112135+RiderLty@users.noreply.github.com>
2025-04-07 10:44:21 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 [ -f "/etc/nginx/conf.d/emby.conf.template" ] && python src/docker.init.py
-command -v nginx >/dev/null 2>&1 && nginx && uv run src/main.py || uv run src/main.py
+command -v nginx >/dev/null 2>&1 && nginx && python src/main.py || python src/main.py
